850dbeab5b78c3138168015e1bb70e8789ca656b
nhpf <nicholas@hopf.pt>
2023-05-01 02:31:19 +0100

    Basic setup for assignment 2

---
+++ assignment_2/README.md
@@ -0,0 +1,28 @@
+# Environment configuration
+
+Setup a virtual environment and install the project dependencies:
+```bash
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+Learn more about PyTorch configuration [here](https://pytorch.org/get-started/locally/).
+
+# References
+
+## Input video frames
+Capture frames:
+ - [Project that uses the same dataset](https://github.com/tilemmpon/Singapore-Maritime-Dataset-Frames-Ground-Truth-Generation-and-Statistics)
+
+## Detection
+YOLO:
+ - [YOLOv4 paper](https://arxiv.org/pdf/2004.10934.pdf)
+ - [YOLOv4 GitHub](https://github.com/AlexeyAB/darknet)
+ - [YOLOv7 GitHub](https://github.com/WongKinYiu/yolov7)
+
+Transfer learning:
+ - [Pytorch quickstart](https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html)
+
+## Multi-object Tracking
+DeepSORT:
---
+++ assignment_2/capture_frames.py
@@ -0,0 +1,59 @@
+import cv2
+from scipy.io import loadmat
+
+
+video_dir = "/externo/cv_data/NIR/Videos/"
+matrix_dir = "/externo/cv_data/NIR/ObjectGT/"
+
+matrix = matrix_dir + "MVI_1520_NIR_ObjectGT.mat"
+mtx = loadmat(matrix)
+
+# num_frames = len(mtx['structXML'][0])
+# for frame_index in range(num_frames):
+#     boxes = mtx["structXML"][0]['BB'][frame_index]
+#     for box in boxes:
+#         print(box)
+
+# Create a video capture object, in this case we are reading the video from a file
+vid_capture = cv2.VideoCapture(video_dir + 'MVI_1520_NIR.avi')
+
+if not vid_capture.isOpened():
+    print("Error opening the video file")
+
+# Read fps and frame count
+else:
+    # Get frame rate information
+    fps = vid_capture.get(cv2.CAP_PROP_FPS)
+    print('Frames per second : ', fps, 'FPS')
+
+    # Get frame count
+    frame_count = vid_capture.get(cv2.CAP_PROP_FRAME_COUNT)
+    print('Frame count : ', frame_count)
+
+frame_index = 0
+while vid_capture.isOpened():
+    # vid_capture.read() methods returns a tuple, first element is a bool
+    # and the second is a frame
+    ret, frame = vid_capture.read()
+    if ret:
+
+        boxes = mtx["structXML"][0]['BB'][frame_index]
+        for box in boxes:
+            print(box)
+            x, y, w, h = box
+            cv2.rectangle(frame, (int(x), int(y)), (int(x+w), int(y+h)), (255, 0, 0), 2)
+
+        cv2.imshow('Frame', frame)
+
+        # 20 milliseconds
+        key = cv2.waitKey(20)
+        frame_index += 1
+
+        if key == ord('q'):
+            break
+    else:
+        break
+
+# Release the video capture object
+vid_capture.release()
+cv2.destroyAllWindows()
